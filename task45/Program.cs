0738438daa775e2a1392d9e280ef7bb7a8e66f07
Alex Ru <Alex.ru.tver@gmail.com>
2023-02-17 01:02:21 +0300

    Дублирование массива с помощъю поэлементного копирования

---
+++ task45/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 45: Напишите программу, которая будет создавать
+// копию заданного массива с помощью поэлементного
+// копирования.
+
+//Console.Clear();
+
+int[] GetArray(int size, int minValue, int maxValue)      // minValue - мин значение/maxValue - максимальное/ size -размер массива
+{
+    int[] array = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = new Random().Next(minValue, maxValue + 1);  //каждому элементумассива присваиваем значение
+    }                                                           // от мин до макс значениий
+    return array;
+}
+
+int[] GetArrayCopy(int[] inArray)
+{
+    int[] arrayCopy = new int[inArray.Length];
+    for (int i = 0; i < inArray.Length; i++)
+    {
+        arrayCopy[i] = inArray[i];
+    }
+    return arrayCopy;
+}
+
+int[] array = GetArray(5, 1, 9);
+System.Console.WriteLine(string.Join(", ", array));
+int[] array2 = (GetArrayCopy(array));
+System.Console.WriteLine(string.Join(", ", array2));
