381c1bf92c54de5013c6f118b3c5a45e36ff2dd9
AlexRu <Alex.Ru.Tver@gmail.com>
2023-02-28 17:36:21 +0300

    Сортировка строк двумерного массива

---
+++ HomeWork6Seminar/task54/Program.cs
@@ -0,0 +1,55 @@
+﻿// Задача 54: Задайте двумерный массив. Напишите программу, которая
+// упорядочит по убыванию элементы каждой строки двумерного массива.
+
+Console.Clear();
+
+int[,] GetArray2D(int a, int b, int minVolue, int maxVolue)
+{
+    int[,] array = new int[a, b];
+    for (int i = 0; i < a; i++)
+    {
+        for (int j = 0; j < b; j++)
+        {
+            array[i, j] = new Random().Next(minVolue, maxVolue + 1);
+        }
+    }
+    return array;
+}
+
+void PrintAraay2D(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]}\t ");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+void SortArray(int[,] array)   // сортируем массив   1,1,2,2,2,3,3 и тд
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(1); k++)
+            {
+                if (array[i, j] > array[i, k])
+                {
+                    int temp = array[i, k];
+                    array[i, k] = array[i, j];
+                    array[i, j] = temp;
+                }
+            }
+        }
+
+    }
+}
+
+int[,] testArray = GetArray2D(5, 5, 1, 9);
+PrintAraay2D(testArray);
+System.Console.WriteLine();
+SortArray(testArray);
+PrintAraay2D(testArray);
